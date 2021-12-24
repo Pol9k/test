b6b80df4a488f99895d8376513f35650830e465c
Andrii <example@gmail.com>
2021-12-24 09:50:35 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,44 @@
+
+'use strict';
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+// console.log(number);1
+
+
+let arr = ['pl.jpg', 6, 'orange.bmp', []];
+
+// console.log(arr);
+// console.log(arr[2]);
+
+// alert('Hello World!');
+// const result = confirm('Are you here?');
+
+// const answer = prompt('Вам є 18?', '18');
+
+// console.log('Are you 18? ' + typeof(answer))
+
+// const answers = [];
+
+// answers[0] = prompt('How you first name? ' + '' );
+// answers[1] = prompt('How you second name? ' + '' );
+// answers[2] = prompt('How old are you? ' + '' );
+
+// document.write(answers);
+
+// Интерполяция
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category }`)
+
+// Инкримент Дикримент
+let incr = 10,
+    decr = 10;
+++incr;
+--decr;
+console.log(incr);
+console.log(decr);
+
+console.log(5%2)
